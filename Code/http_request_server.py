25c5fe9cc2670fa2e548ba506d08086ef5369450
Elijah <33730055+elij123@users.noreply.github.com>
2023-02-17 22:34:36 -0500

    Update HTTP Request Server

--- Code/http_request_server.py
+++ Code/http_request_server.py
@@ -9,53 +9,73 @@ import sys
 import os
 from datetime import datetime
 
+# Grammar for the hostname of the Host field
+h16 = "[0-9A-Fa-f]{1,4}"
+dec_octet = "([0-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])"
+IPv4addr = f"{dec_octet}.{dec_octet}.{dec_octet}.{dec_octet}"
+ls32 = f"({h16}:{h16}|{IPv4addr})"
+ipv6_sub1 = f"({h16}:){{6}}{ls32}"
+ipv6_sub2 = f"::({h16}:){{5}}{ls32}"
+ipv6_sub3 = f"({h16})?::({h16}:){{4}}{ls32}"
+ipv6_sub4 = f"(({h16}:){{0,1}}{h16})?::({h16}:){{3}}{ls32}"
+ipv6_sub5 = f"(({h16}:){{0,2}}{h16})?::({h16}:){{2}}{ls32}"
+ipv6_sub6 = f"(({h16}:){{0,3}}{h16})?::({h16}:){ls32}"
+ipv6_sub7 = f"(({h16}:){{0,4}}{h16})?::{ls32}"
+ipv6_sub8 = f"(({h16}:){{0,5}}{h16})?::{h16}"
+ipv6_sub9 = f"(({h16}:){{0,6}}{h16})?::"
+IPv6addr = f"{ipv6_sub1}|{ipv6_sub2}|{ipv6_sub3}|{ipv6_sub4}|\
+{ipv6_sub5}|{ipv6_sub6}|{ipv6_sub7}|{ipv6_sub8}|{ipv6_sub9}"
+IPvfuture = "v[0-9A-Fa-f]+.([\w.\-~]|[!$&'()*+,;=]|[:])?"
+IP_literal = f"\[({IPv6addr}|{IPvfuture})\]"
+reg_name = "([\w.\-~]|[%][0-9A-Fa-f][0-9A-Fa-f]|[!$&'()*+,;=])*"
+host = re.compile(f"{IP_literal}|{IPv4addr}|{reg_name}", re.ASCII)
+
+# Grammar for the port number of the Host field
+port = re.compile("[\d]*", re.ASCII)
+
 tz_NY = pytz.timezone("America/New_York")
+# Minor HTTP version
 server_minor_ver = 1
-head_flag = 0
+# Global flag for HEAD HTTP method
+http_head_flag = 0
 token_regex = re.compile("[!#$%&’*+\-.^‘|~\w]+", re.ASCII)
 # Regex for the URI in the request
 absolute_path_regex = re.compile(
-    "(/ ( [\w.\-~] | [%][0-9A-Fa-f][0-9A-Fa-f] | [!$&'()*+,;=] |\
-     [:] | [@] )* )+",
-    re.X,
+    "(/([\w.\-~]|[%][0-9A-Fa-f][0-9A-Fa-f]|[!$&'()*+,;=]|[:]|[@])*)+", re.ASCII,
 )
 query_regex = re.compile(
-    "( [\w.\-~] | [%][0-9A-Fa-f][0-9A-Fa-f] | [!$&'()*+,;=] |\
-     [:] | [@] | [/] | [?] )*",
-    re.X,
+    "([\w.\-~]|[%][0-9A-Fa-f][0-9A-Fa-f]|[!$&'()*+,;=]|[:]|[@]|[/]|[?])*", re.ASCII,
 )
 body_index = None
 http_response_to_send = None
 CRLF = "\r\n"
 
-
+# Defining exceptions for HTTP errors
 class BadRequestException(Exception):
-    "Raised if the HTTP request is ill-formed"
-    pass
+    print("the HTTP request is ill-formed")
+
+
+class VersionNotSupported(Exception):
+    print("The HTTP version of the request is not supported")
+
+
+class MethodNotImplemented(Exception):
+    print("The HTTP method in the request is not supported")
 
 
 # Parses HTTP request
 def http_request_message(input_http_str: str):
     global http_response_to_send
-    try:
-        first_CRLF = input_http_str.find(CRLF)
-        if first_CRLF != -1:
-            request_start_line_http(input_http_str[:first_CRLF])
-            header_block_http(input_http_str[first_CRLF + 2 :])
-        else:
-            raise BadRequestException
-    except BadRequestException:
-        http_response_to_send = http_response_400()
-    except:
-        http_response_to_send = http_response_500()
-    finally:
-        pass
-    # Sends the 200 OK response if no exceptions are thrown
-    if http_response_to_send == None:
-        if head_flag:
-            http_response_to_send = http_response_200_head()
-        else:
-            http_response_to_send = http_response_200()
+    first_CRLF = input_http_str.find(CRLF)
+    if first_CRLF != -1:
+        request_start_line_http(input_http_str[:first_CRLF])
+        header_block_http(input_http_str[first_CRLF + 2 :])
+    return http_response_to_send
+    # if http_response_to_send == None:
+    #     if http_head_flag:
+    #         http_response_to_send = http_response_200_head()
+    #     else:
+    #         http_response_to_send = http_response_200()
 
 
 # Parses HTTP Request Line
@@ -70,7 +90,7 @@ def request_start_line_http(input_str: str):
 # HTTP responses for different HTTP request
 def http_method(input: str):
     global http_response_to_send
-    global head_flag
+    global http_head_flag
     try:
         if re.fullmatch(token_regex, input) != None:
             if input == "GET":
@@ -83,9 +103,9 @@ def http_method(input: str):
                 pass
             elif input == "HEAD":
                 # Sets the flag to send the response for HEAD request
-                head_flag = 1
+                http_head_flag = 1
             else:
-                raise BadRequestException
+                raise MethodNotImplemented
         else:
             raise BadRequestException
     except BadRequestException:
@@ -108,13 +128,13 @@ def request_target(input_str: str):
                     input_str_seg = input_str.split("?")
                     URI_path = input_str_seg[0]
                     query_str = input_str_seg[1]
-                    if re.match(absolute_path_regex, URI_path) == None:
+                    if re.fullmatch(absolute_path_regex, URI_path) == None:
                         raise BadRequestException
-                    if re.match(query_regex, query_str) == None:
+                    if re.fullmatch(query_regex, query_str) == None:
                         raise BadRequestException
                 else:
                     URI_path = input_str
-                    if re.match(absolute_path_regex, URI_path) == None:
+                    if re.fullmatch(absolute_path_regex, URI_path) == None:
                         raise BadRequestException
         else:
             raise BadRequestException
@@ -135,8 +155,12 @@ def http_version(input_str: str):
             segments = input_str.split("/")
             version_no = segments[1]
             version_no_seg = version_no.split(".")
-            if int(version_no_seg[0]) > 1:
-                raise BadRequestException
+            if (
+                int(version_no_seg[0]) != 1
+                or int(version_no_seg[1]) > 1
+                or int(version_no_seg[1]) < 0
+            ):
+                raise VersionNotSupported
             server_minor_ver = version_no_seg[1]
         else:
             raise BadRequestException
@@ -146,36 +170,6 @@ def http_version(input_str: str):
         http_response_to_send = http_response_500()
     finally:
         pass
-
-
-def host_name_verify(name):
-    # Grammar for the hostname of the Host field
-    h16 = "[0-9A-Fa-f]{1,4}"
-    dec_octet = "([0-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])"
-    IPv4addr = f"{dec_octet}.{dec_octet}.{dec_octet}.{dec_octet}"
-    ls32 = f"({h16}:{h16}|{IPv4addr})"
-    ipv6_sub1 = f"({h16}:){{6}}{ls32}"
-    ipv6_sub2 = f"::({h16}:){{5}}{ls32}"
-    ipv6_sub3 = f"({h16})?::({h16}:){{4}}{ls32}"
-    ipv6_sub4 = f"(({h16}:){{0,1}}{h16})?::({h16}:){{3}}{ls32}"
-    ipv6_sub5 = f"(({h16}:){{0,2}}{h16})?::({h16}:){{2}}{ls32}"
-    ipv6_sub6 = f"(({h16}:){{0,3}}{h16})?::({h16}:){ls32}"
-    ipv6_sub7 = f"(({h16}:){{0,4}}{h16})?::{ls32}"
-    ipv6_sub8 = f"(({h16}:){{0,5}}{h16})?::{h16}"
-    ipv6_sub9 = f"(({h16}:){{0,6}}{h16})?::"
-    IPv6addr = f"{ipv6_sub1}|{ipv6_sub2}|{ipv6_sub3}|{ipv6_sub4}|\
-    {ipv6_sub5}|{ipv6_sub6}|{ipv6_sub7}|{ipv6_sub8}|{ipv6_sub9}"
-    IPvfuture = "v[0-9A-Fa-f]+.([\w.\-~]|[!$&'()*+,;=]|[:])?"
-    IP_literal = f"\[({IPv6addr}|{IPvfuture})\]"
-    reg_name = "([\w.\-~]|[%][0-9A-Fa-f][0-9A-Fa-f]|[!$&'()*+,;=])*"
-    host = re.compile(f"{IP_literal}|{IPv4addr}|{reg_name}", re.ASCII)
-    return re.match(host, name)
-
-
-def host_port_verify(port_no):
-    # Grammar for the port number of the Host field
-    port = re.compile("[\d]*", re.ASCII)
-    return re.match(port, port_no)
 
 
 # Parses Header field
@@ -189,7 +183,7 @@ def header_block_http(header_block_str: str):
         while header_CRLF_index != -1:
             header_temp_str = header_block_str[: header_CRLF_index + 2]
             if (
-                re.match(
+                re.fullmatch(
                     "[!#$%&’*+\-.^‘|~\w]+:( |\t)*([\S ](( |\t)+[\S ])?)*( |\t)*" + CRLF,
                     header_temp_str,
                     re.ASCII,
@@ -201,8 +195,8 @@ def header_block_http(header_block_str: str):
                 temp = header_temp_str.split(":")
                 if host_str == None:
                     # Checks for existence of 'host' header field
-                    host_str = re.match("host", temp[0], re.IGNORECASE).string
-                http_headers_dict[temp[0]] = temp[1]
+                    host_str = re.fullmatch("host", temp[0], re.IGNORECASE).string
+                http_headers_dict[temp[0]] = temp[1].strip()
             # Checks for second CRLF to indicate end of header block
             if header_block_str[header_CRLF_index + 2 : header_CRLF_index + 4] == CRLF:
                 body_index = header_CRLF_index + 4
@@ -237,27 +231,108 @@ def header_block_http(header_block_str: str):
         pass
 
 
+def host_name_verify(name):
+    return re.fullmatch(host, name)
+
+
+def host_port_verify(port_no):
+    return re.fullmatch(port, port_no)
+
+
+# Request Methods
+def GET_request(filepath):
+    try:
+        requested_file = open(filepath, "r")
+    except FileNotFoundError:
+        print("The file requested is not found")
+        http_response_404()
+    except PermissionError:
+        print("Not Allowed to access the file")
+        http_response_403()
+    except:
+        http_response_500()
+    else:
+        requested_file.read()
+        requested_file.close()
+    pass
+
+
+def PUT_request(filepath, content):
+    try:
+        requested_file = open(filepath, "w")
+    except FileNotFoundError:
+        print("The file requested is not found")
+        http_response_404()
+    except PermissionError:
+        print("Not Allowed to access the file")
+        http_response_403()
+    except:
+        http_response_500()
+    else:
+        requested_file.write(content)
+        requested_file.close()
+
+
+def POST_request():
+    pass
+
+
+def DELETE_request(filepath):
+    try:
+        os.remove(filepath)
+    except FileNotFoundError:
+        print("The file requested is not found")
+        http_response_404()
+    except PermissionError:
+        print("Not Allowed to access the file")
+        http_response_403()
+    except:
+        http_response_500()
+
+
 # HTTP responses for 200(OK)
 def http_response_200():
-    resp = f"HTTP/1.{server_minor_ver} 200 OK\r\nServer: Apache/2.4.54 (Unix)\r\nContent-Location: index.html.en\r\n\Accept-Ranges: bytes\r\nContent-Length: 45\r\nContent-Type: text/html\r\n\r\n<html><body><h1>It works!</h1></body></html>"
+    resp = f"HTTP/1.{server_minor_ver} 200 OK\r\n"
+    resp += "Server: Apache/2.4.54 (Unix)\r\n"
+    resp += "Content-Location: index.html.en\r\n"
+    resp += "Accept-Ranges: bytes\r\n"
+    resp += "Content-Length: 45\r\n"
+    resp += "Content-Type: text/html\r\n\r\n"
+    resp += "<html><body><h1>It works!</h1></body></html>"
     return resp
 
 
 # Bad Request
 def http_response_400():
-    resp = f"HTTP/1.{server_minor_ver} 400 Bad Request\r\nServer: Apache/2.4.54 (Unix)\r\nContent-Length: 226\r\nConnection: close\r\nContent-Type: text/html; charset=iso-8859-1\r\n\r\n<html><body><h1>400:Bad Request</h1></body></html>"
+    resp = f"HTTP/1.{server_minor_ver} 400 Bad Request\r\n"
+    resp += "Server: Apache/2.4.54 (Unix)\r\n"
+    resp += "Content-Location: index.html.en\r\n"
+    resp += "Accept-Ranges: bytes\r\n"
+    resp += "Content-Length: 45\r\n"
+    resp += "Content-Type: text/html\r\n\r\n"
+    resp += "<html><body><h1>It works!</h1></body></html>"
     return resp
 
 
 # HTTP HEAD Response
 def http_response_200_head():
-    resp = f"HTTP/1.{server_minor_ver} 200 OK\r\nServer: Apache/2.4.54 (Unix)\r\nContent-Location: index.html.en\r\nAccept-Ranges: bytes\r\nContent-Length: 45\r\nContent-Type: text/html\r\n\r\n"
+    resp = f"HTTP/1.{server_minor_ver} 200 OK\r\n"
+    resp += "Server: Apache/2.4.54 (Unix)\r\n"
+    resp += "Content-Location: index.html.en\r\n"
+    resp += "Accept-Ranges: bytes\r\n"
+    resp += "Content-Length: 45\r\n"
+    resp += "Content-Type: text/html\r\n\r\n"
     return resp
 
 
 # Internal Server Error
 def http_response_500():
-    resp = f"HTTP/1.{server_minor_ver} 500 Internal Server Error\r\nServer: Apache/2.4.54 (Unix)\r\nContent-Length: 226\r\nConnection: close\r\nContent-Type: text/html; charset=iso-8859-1\r\n\r\n<html><body><h1>500: Internal Server Error</h1></body></html>"
+    resp = f"HTTP/1.{server_minor_ver} 500 Internal Server Error\r\n"
+    resp += "Server: Apache/2.4.54 (Unix)\r\n"
+    resp += "Content-Length: 226\r\n"
+    resp += "Connection: close\r\n"
+    resp += "Content-Type: text/html; charset=iso-8859-1\r\n\r\n"
+    resp += "<html><body><h1>500: Internal Server Error</h1></body></html>"
     return resp
 
 
@@ -276,7 +351,12 @@ def http_response_404():
     pass
 
 
-# Method Not Implemented
+# POST - No Content Length
+def http_response_411():
+    pass
+
+
+# HTTP Method Not Implemented
 def http_response_501():
     pass
 
@@ -291,21 +371,28 @@ def https_conn_handler(conn, x509, privatekey):
         warnings.simplefilter(action="ignore", category=DeprecationWarning)
         context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
     context.verify_mode = ssl.CERT_NONE
-    context.load_cert_chain(
-        certfile="./project.crt", keyfile="./project.key", password=None
-    )
+    context.load_cert_chain(certfile=x509, keyfile=privatekey, password=None)
     tls_conn = context.wrap_socket(conn, server_side=True)
-    resp = None
+    recv_req = tls_conn.recv(2048)
+    while recv_req:
+        resp += recv_req.decode("UTF-8")
+        recv_req = tls_conn.recv(2048)
+    resp = http_request_message(recv_req)
     tls_conn.send(resp.encode("UTF-8"))
     tls_conn.close()
 
 
 def http_conn_handler(conn):
-    resp = None
+    recv_req = conn.recv(2048)
+    while recv_req:
+        resp += recv_req.decode("UTF-8")
+        recv_req = conn.recv(2048)
+    resp = http_request_message(recv_req)
     conn.send(resp.encode("UTF-8"))
     conn.close()
 
 
+# Logger
 def http_request_logger(request):
     with open("http_log.txt", "w") as http_log:
         if request.find(CRLF) != -1:
